edecadd2fced1d926aec6dd74143a3b55679e583
khadiravana-belagavi <khadiravana.belagavi@research.iiit.ac.in>
2021-06-03 23:54:01 +0530

    undo commit

--- app/config.py
+++
@@ -1,9 +0,0 @@
-
-# directory containing metadata files for each layout. Checkout docs for the metadata information.
-# All json files in the `metaDataDir` must be in some specified format.
-
-metaDataDir = '/home/user/hdia/hindola/hindola/visualizer/icdar-visualizer/app/tests/metaData' 
-docVisorRepo = 'https://github.com/khadiravana-belagavi/icdar-visualizer'
-
-
-
